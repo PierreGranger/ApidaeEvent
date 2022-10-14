4b1ea13c1679c8e389d5181baf432a9e0f6f0b80
Pierre Granger <pierre.granger@apidae-tourisme.com>
2022-10-14 15:06:15 +0200

    Erreur permission /gen/

--- public/gen/auth.inc.php
+++ public/gen/auth.inc.php
@@ -11,26 +11,6 @@ $ApidaeSso = new ApidaeSso($configApidaeSso,$_SESSION['ApidaeSso']) ;
 if ( isset($_GET['logout']) ) $ApidaeSso->logout() ;
 if ( isset($_GET['code']) && ! $ApidaeSso->connected() ) $ApidaeSso->getSsoToken($_GET['code']) ;
 
-ini_set('display_errors',0) ;
-error_reporting(E_ALL) ;
-
-$droits = Array('permissions'=>Array('ADMIN_DEPARTEMENTAL')) ;
-
-if ( $ApidaeSso->connected() )
-{
-    $utilisateurApidae = $ApidaeSso->getUserProfile() ;
-    $apidaeMembres = new PierreGranger\ApidaeMembres(array_merge(
-        Array('debug'=>false,'timer'=>true),
-        $configApidaeMembres
-    )) ;
-    $usr = $apidaeMembres->getUserById($utilisateurApidae['id']) ;
-    foreach ( $droits['permissions'] as $p )
-    {
-        if ( ! in_array($p,$usr['permissions']) && ! preg_match('#sipea\.fr$#',$usr['contact']['eMail']) )
-            die('<p>Vous ne disposez pas des permissions suffisantes ('.$p.' not in ('.implode(', ',$usr['permissions']).')</p>') ;
-    }
-}
-
 if ( ! $ApidaeSso->connected() )
 {
     echo $ApidaeSso->form() ;
