3d55ced9b477c924a8564fc1c343949e39f52ac7
Pierre Granger <pierre.granger@apidae-tourisme.com>
2023-02-02 16:47:30 +0100

    pb scripts

--- scripts/cache.php
+++ scripts/cache.php
@@ -1,6 +1,6 @@
 <?php
 
-    require_once(realpath(dirname(__FILE__)).'/../requires.inc.php') ;
+    require_once(realpath(dirname(__FILE__)).'/../src/requires.inc.php') ;
 
     /**
      * Fonctions qui utilisent du cache
--- scripts/flush.php
+++ scripts/flush.php
@@ -1,5 +1,5 @@
 <?php
 
-    require_once(realpath(dirname(__FILE__)).'/../requires.inc.php') ;
+    require_once(realpath(dirname(__FILE__)).'/../src/requires.inc.php') ;
 
     $apidaeEvent->flush() ;
